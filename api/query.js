7c5288234c3bdec88e0c1afaae259383a86341c5
MasonWang025 <mason.wang0025@gmail.com>
2022-07-13 13:36:23 -0700

    added all files

---
+++ api/query.js
@@ -0,0 +1,45 @@
+const express = require("express");
+
+const router = express.Router();
+
+router.get("/", (req, res) => {
+    q = req.query.q
+
+    if (q == '')
+        req.json({
+            "code": "EINVAL",
+            "error": "missing or invalid parameter q"
+        })
+
+    reply = {
+        "result": "ok",
+        "candidates": [
+            {
+                "code": [
+                    "$dialogue",
+                    "@org.thingpedia.dialogue.transaction",
+                    ".",
+                    "execute",
+                    ";",
+                    "@com.asd",
+                    ".",
+                    "asd",
+                    "(",
+                    ")",
+                    ";"
+                ],
+                "score": 1
+            }
+        ],
+        "tokens": q.split(" "),
+        "entities": {},
+        "intent": {
+            "command": 1,
+            "other": 0,
+            "ignore": 0
+        }
+    }
+    res.json(reply);
+});
+
+module.exports = router;
---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const path = require('path')
+
+const app = express()
+
+const PORT = process.env.PORT || 8400
+
+app.get("/", (req, res) => res.sendFile(path.join(__dirname, 'public', 'index.html'))); //homepage route
+
+// body parser middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// template rendering
+app.get("/", (req, res) => res.render("index")); //homepage route
+
+// members api routes
+app.use("/query", require("./api/query"));
+
+app.listen(PORT, () => {
+    console.log(`Server started on port ${PORT}`);
+});
